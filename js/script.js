32a3cf146b90d492bcb179e89690cbd45071ab1b
Jasminbrusberg <J-brusberg@hotmail.com>
2026-03-04 22:50:16 +0100

    opgave 4

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const animals =[
+    {name:"simba", species:"løve", age:"5", food:"kød"}
+  {name:"Dumbo", species:"Elefant", age:"8", food:"Blade og frugt"}
+    {name:"Gerald", species:"Giraf", age:"6", food:"Blade fra høje træer"}
+];
+
+
